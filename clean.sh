6e6a527955fe71ce3bbe5d09041b23f44e9fe2a5
Argentum <bro.alexeev@inbox.ru>
2024-01-04 01:05:12 +0700

    complete

---
+++ clean.sh
@@ -0,0 +1,5 @@
+sudo xi --sync && sudo xi -Su && sudo xbps-remove --clean-cache && sudo xbps-remove -yo && sudo vkpurge rm all
+sudo rm -rf tmp/*
+sudo rm -rf /var/log/*
+sudo rm -rf /var/cache/xbps/*
+rm -rf ~/.cache
